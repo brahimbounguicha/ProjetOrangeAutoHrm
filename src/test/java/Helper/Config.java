2e94f6623089e84887eaf677727bbe144095e4f2
brahimbounguicha <44786262+brahimbounguicha@users.noreply.github.com>
2024-04-14 19:33:41 +0200

    je cahnge config.java

--- src/test/java/Helper/Config.java
+++ src/test/java/Helper/Config.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebDriver;
 public class Config {
 	
 	public static WebDriver driver ;
+public static WebDriver driver ;
+public static WebDriver driver ;
 	
 	
 	public static void confChrome() {
